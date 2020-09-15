ce36ff5bcc99ca000aa42dde1a1a22ff2af24bc1
0xchase <chasekanipe@gmail.com>
2020-09-15 14:44:18 -0400

    additions

--- ideas.md
+++ ideas.md
@@ -4,3 +4,4 @@
  - Print bil or bir in a more readable form
  - Do function detection, integrate with radare2
  - Debugger, then symbolic debugger
+ - Plugin to read memory from debugger state. Could, for example, execute kernel to certain point, break, then run BAP which reads from this frozen debugger state. 
--- plugins/basic/simple.ml
+++ plugins/basic/simple.ml
@@ -1,11 +1,56 @@
 open Core_kernel
 open Bap_main
 open Bap.Std
+open Format
 
-let main proj =
-  print_endline "My analysis is running!";
-  proj
+let counter = object
+        inherit [int * int] Term.visitor
+        method! enter_term _ _ (jmps,total) = jmps,total+1
+        method! enter_jmp _ (jmps,total) = jmps+1, total
+end
+
+(* Print ratio of jmps to instructions *)
+let ratio proj = 
+        let jmps, total = counter#run (Project.program proj) (0, 0) in
+        printf "ratio = %d/%d = %g\n" jmps total (float jmps /. float total)
+
+(* Print architecture information *)
+let info proj = 
+      printf "Running plugin\n";
+      let a =
+        match Project.arch proj with
+        | #Arch.arm -> "ARM"
+        | #Arch.x86 -> "x86"
+        | #Arch.mips -> "MIPS"
+        | _ -> "Unknown"
+        in printf "Detected architecture: %s\n" a
+
+let print_insns proj =
+        Seq.iter (Disasm.insns (Project.disasm proj)) ~f:(fun (mem,insn) ->
+                Printf.printf "%s\n"
+                        (Insn.asm insn))
+
+let print_subs proj =
+        (Disasm.insns (Project.disasm proj))
+
+let zero_collector = object
+        inherit [Tid.Set.t] Term.visitor
+        method! enter_def t zeros = match Def.rhs t with
+        | Bil.Int x when Word.is_zero x -> Set.add zeros (Term.tid t)
+        | _ -> zeros
+end
+
+let test proj =
+        print_endline "Doing some stuff";
+        zero_collector#run (Project.program proj) Tid.Set.empty |> 
+        Set.iter ~f:(Format.printf "%a@\n" Tid.pp)
+        
+(* Print sections *)
+let print_sections proj =
+        Project.memory proj |> Memmap.to_sequence |> Seq.iter ~f:(fun (mem,x) ->
+                Option.iter (Value.get Image.section x) ~f:(fun name ->
+                        printf "Section: %s\n" name))
 
 let () = Extension.declare @@ fun _ctxt ->
-   Project.register_pass main
-
+        Project.register_pass' test;
+        Ok ()
